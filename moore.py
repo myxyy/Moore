401f15d041dacc4572f1615cc062446c01d404f8
myxyy <m.myxomatosis@gmail.com>
2025-12-03 06:50:25 +0000

    adjust and print parameters

--- moore.py
+++ moore.py
@@ -11,9 +11,9 @@ parser.add_argument("--degree", type=int, default=57, choices=[7, 57], help="Deg
 parser.add_argument("--batch_size", type=int, default=1, help="Batch size for training (default: 1)")
 parser.add_argument("--num_steps", type=int, default=1000000, help="Number of optimization steps (default: 1000000)")
 parser.add_argument("--lr", type=float, default=0.4, help="Learning rate for the optimizer (default: 0.4)")
-parser.add_argument("--diagonal_weight", type=float, default=1e-2, help="Weight for the diagonal loss component (default: 1e-2)")
+parser.add_argument("--diagonal_weight", type=float, default=5e-3, help="Weight for the diagonal loss component (default: 5e-3)")
 parser.add_argument("--target_noise_scale", type=float, default=0.1, help="Scale of the noise added to the target (default: 0.1)")
-parser.add_argument("--regularity_weight", type=float, default=100.0, help="Weight for the regularity loss component (default: 100.0)")
+parser.add_argument("--regularity_weight", type=float, default=10.0, help="Weight for the regularity loss component (default: 10.0)")
 parser.add_argument("--regularity_noise_scale", type=float, default=0.1, help="Scale of the noise added to the target (default: 0.1)")
 parser.add_argument("--check_interval", type=int, default=1000, help="Interval for checking progress (default: 1000)")
 args = parser.parse_args()
@@ -27,6 +27,12 @@ check_interval = args.check_interval
 target_noise_scale = args.target_noise_scale
 regularity_noise_scale = args.regularity_noise_scale
 regularity_weight = args.regularity_weight
+print(f"degree: {degree}")
+print(f"lr: {lr}")
+print(f"diagonal_weight: {diagonal_weight}")
+print(f"target_noise_scale: {target_noise_scale}")
+print(f"regularity_weight: {regularity_weight}")
+print(f"regularity_noise_scale: {regularity_noise_scale}")
 
 torch.set_printoptions(precision=1, edgeitems=1000, linewidth=1000)
 
@@ -61,78 +67,75 @@ mask = moore_mask(degree).detach().to(device)
 target = moore_target(degree).detach().to(device)
 
 class MooreModel(nn.Module):
-    def __init__(self, degree: int):
-        super(MooreModel, self).__init__()
+    def __init__(self, batch_size: int, degree: int):
+        super().__init__()
         self.degree = degree
-        self.mask = moore_mask(degree).detach().to(device)
-    def forward(self, params: torch.Tensor) -> torch.Tensor:
-        adj_mat = F.sigmoid(params)
+        self.mask = nn.Parameter(moore_mask(degree), requires_grad=False)
+        self.params = nn.Parameter(torch.randn(batch_size, degree * (degree - 1), degree * (degree - 1)))
+
+    def forward(self) -> torch.Tensor:
+        adj_mat = F.sigmoid(self.params)
         adj_mat = F.pad(adj_mat, (0, 1 + self.degree, 0, 1 + self.degree), value=1.0).triu()
         adj_mat = adj_mat + adj_mat.transpose(-2, -1)
         adj_mat = adj_mat * self.mask
         return adj_mat
 
-model = MooreModel(degree).to(device)
+model = MooreModel(batch_size, degree).to(device)
 target = target[None,:,:].expand(batch_size, -1, -1).to(device)
 size = 1 + degree + (degree - 1) * degree
 
-is_success = False
-while not is_success:
-    params = torch.randn(batch_size, degree * (degree - 1), degree * (degree - 1)).to(device)
-    params = nn.Parameter(params)
-    optimizer = partial_optimizer(params=[params])
-    t = tqdm(range(num_steps), dynamic_ncols=True)
-    for step in t:
-        optimizer.zero_grad()
-        #noise = torch.randn_like(params) / size
-        #params.data.add_(noise)
-        adj_mat_hat = model(params)
-        target_hat = torch.matmul(adj_mat_hat, adj_mat_hat) + adj_mat_hat
-        target_with_noise = target + torch.randn_like(target_hat) * target_noise_scale
-        mse = F.mse_loss(target_hat, target_with_noise, reduction='none')
+optimizer = partial_optimizer(model.parameters())
+t = tqdm(range(num_steps), dynamic_ncols=True)
+for step in t:
+    optimizer.zero_grad()
 
-        mse_diagonal = mse.diagonal(dim1=1, dim2=2)
-        loss_diagonal = mse_diagonal.sum(dim=1) / size
+    adj_mat_hat = model()
+    target_hat = torch.matmul(adj_mat_hat, adj_mat_hat) + adj_mat_hat
+    target_with_noise = target + torch.randn_like(target_hat) * target_noise_scale
+    mse = F.mse_loss(target_hat, target_with_noise, reduction='none')
 
-        eye = torch.eye(size).to(mse.device)
-        mse_without_diag = mse * (1 - eye)[None, :, :]
-        loss_without_diag = mse_without_diag.sum(dim=(1, 2)) / (size * (size - 1))
+    mse_diagonal = mse.diagonal(dim1=1, dim2=2)
+    loss_diagonal = mse_diagonal.sum(dim=1) / size
+
+    eye = torch.eye(size).to(mse.device)
+    mse_without_diag = mse * (1 - eye)[None, :, :]
+    loss_without_diag = mse_without_diag.sum(dim=(1, 2)) / (size * (size - 1))
 
 
-        if regularity_weight > 0:
-            adj_mat_hat_param_part = adj_mat_hat[:, :degree * (degree - 1), :degree * (degree - 1)] + torch.eye(degree * (degree - 1)).to(adj_mat_hat.device)[None, :, :]
-            adj_mat_hat_param_part_reshape = adj_mat_hat_param_part.reshape(batch_size, degree * (degree - 1), degree, degree - 1)
-            col_sum = adj_mat_hat_param_part_reshape.sum(dim=3)
-            regularity_loss = F.mse_loss(col_sum, torch.ones_like(col_sum) + torch.randn_like(col_sum) * regularity_noise_scale, reduction='none').mean(dim=(1,2))
-        else:
-            regularity_loss = 0
+    if regularity_weight > 0:
+        adj_mat_hat_param_part = adj_mat_hat[:, :degree * (degree - 1), :degree * (degree - 1)] + torch.eye(degree * (degree - 1)).to(adj_mat_hat.device)[None, :, :]
+        adj_mat_hat_param_part_reshape = adj_mat_hat_param_part.reshape(batch_size, degree * (degree - 1), degree, degree - 1)
+        col_sum = adj_mat_hat_param_part_reshape.sum(dim=3)
+        regularity_loss = F.mse_loss(col_sum, torch.ones_like(col_sum) + torch.randn_like(col_sum) * regularity_noise_scale, reduction='none').mean(dim=(1,2))
+    else:
+        regularity_loss = 0
         
-        loss_batch = loss_diagonal * diagonal_weight + loss_without_diag + regularity_loss * regularity_weight
+    loss_batch = loss_diagonal * diagonal_weight + loss_without_diag + regularity_loss * regularity_weight
 
-        loss_batch_grad = torch.ones_like(loss_batch)
-        loss_batch.backward(gradient=loss_batch_grad)
-        optimizer.step()
+    loss_batch_grad = torch.ones_like(loss_batch)
+    loss_batch.backward(gradient=loss_batch_grad)
+    optimizer.step()
 
+    with torch.no_grad():
+        min_loss = loss_batch.min().item()
+        min_index = torch.argmin(loss_batch).item()
+        best_adj_mat = model()[min_index]
+        best_target_hat = torch.matmul(best_adj_mat, best_adj_mat) + best_adj_mat
+        diagonal = best_target_hat.diagonal()
+        eye = torch.eye(best_target_hat.size(0)).to(best_target_hat.device)
+        j = best_target_hat * (1 - eye) + eye
+
+    t.set_postfix({
+        'loss': f'{min_loss:.3f}',
+        'd_mean': f'{diagonal.mean().item():.3f}',
+        'd_std': f'{diagonal.std().item():.3f}',
+        'j_mean': f'{j.mean().item():.3f}',
+        'j_std': f'{j.std().item():.3f}',
+    })
+
+    if step % check_interval == 0:
         with torch.no_grad():
-            min_loss = loss_batch.min().item()
-            min_index = torch.argmin(loss_batch).item()
-            min_param = params[min_index]
-            best_adj_mat = model(min_param[None, :, :]).squeeze(0)
-            best_target_hat = torch.matmul(best_adj_mat, best_adj_mat) + best_adj_mat
-            diagonal = best_target_hat.diagonal()
-            eye = torch.eye(best_target_hat.size(0)).to(best_target_hat.device)
-            j = best_target_hat * (1 - eye) + eye
-
-        t.set_postfix({
-            'loss': f'{min_loss:.3f}',
-            'd_mean': f'{diagonal.mean().item():.3f}',
-            'd_std': f'{diagonal.std().item():.3f}',
-            'j_mean': f'{j.mean().item():.3f}',
-            'j_std': f'{j.std().item():.3f}',
-        })
-
-        if step % check_interval == 0:
-            adj_mat_round = torch.round(model(params).detach())
+            adj_mat_round = torch.round(model().detach())
             hat_round = torch.matmul(adj_mat_round, adj_mat_round) + adj_mat_round
             min_index = torch.argmin((hat_round - target).abs().sum(dim=(1, 2)))
             if (hat_round - target)[min_index].abs().sum().item() == 0:
